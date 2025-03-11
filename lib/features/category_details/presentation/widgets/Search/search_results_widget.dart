b98c1cf85a44f9fce20b8b10c3399246970c33a2
Nouran <nouransamer5@gmail.com>
2025-03-11 12:43:37 +0200

    Finish Adjusting for Search

---
+++ lib/features/category_details/presentation/widgets/Search/search_results_widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/widget/error_display_widget.dart';
+import 'package:news_app/features/category_details/presentation/manager/category_cubit.dart';
+import 'package:news_app/features/category_details/presentation/widgets/ArticleItem.dart';
+import 'package:news_app/features/category_details/presentation/widgets/Search/no_results_widget.dart';
+import 'package:news_app/features/category_details/presentation/widgets/showNewsDetails.dart';
+
+class SearchResultsWidget extends StatelessWidget {
+  const SearchResultsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.primary,
+      child: BlocBuilder<CategoryCubit, CategoryState>(
+        buildWhen: (previous, current) =>
+            current is ArticlesLoadedSuccessState ||
+            current is ArticlesErrorState ||
+            current is ArticlesLoadingState,
+        builder: (context, state) {
+          if (state is ArticlesLoadedSuccessState) {
+            final articles = state.articlesEntity.articles;
+            if (articles == null || articles.isEmpty) {
+              return const NoResultsWidget();
+            }
+            return ListView.separated(
+              padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (context) {
+                        return ShowNewsDetails(article: articles[index]);
+                      },
+                    );
+                  },
+                  child: ArticleItem(articleEntity: articles[index]),
+                );
+              },
+              separatorBuilder: (context, index) => SizedBox(height: 10.h),
+              itemCount: articles.length,
+            );
+          } else if (state is ArticlesErrorState) {
+            return ErrorDisplayWidget(errorMessage: state.error);
+          } else {
+            return Center(
+              child: CircularProgressIndicator(color: Theme.of(context).colorScheme.secondary),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
